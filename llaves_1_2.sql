266040afa6b78fbf25cc16ddb6d8b923de05eee6
jimenezsebastian06 <sebastianenriquejimenez@gmail.com>
2026-04-23 19:49:37 -0600

    Modificación llaves 1 y 2: validaciones NULL y cambio de variables

--- llaves_1_2.sql
+++ llaves_1_2.sql
@@ -1,45 +1,53 @@
 DELIMITER //
 
---Llave 1
+-- Llave 1
 
-CREATE FUNCTION fn_cernidor(id INT)
+CREATE FUNCTION fn_cernidor(p_id INT)
 RETURNS BOOLEAN
 DETERMINISTIC
 BEGIN
 
-    DECLARE i INT DEFAULT 2;
-    DECLARE es_primo BOOLEAN DEFAULT TRUE;
+    DECLARE p_i INT DEFAULT 2;
+    DECLARE p_es_primo BOOLEAN DEFAULT TRUE;
 
-    IF id <= 1 THEN
+    IF p_id IS NULL THEN
+        RETURN NULL;
+    END IF;
+
+    IF p_id <= 1 THEN
         RETURN FALSE;
     END IF;
 
-    WHILE i <= SQRT(id) AND es_primo = TRUE DO
-        IF id % i = 0 THEN
-            SET es_primo = FALSE;
+    WHILE p_i <= SQRT(p_id) AND p_es_primo = TRUE DO
+        IF p_id % p_i = 0 THEN
+            SET p_es_primo = FALSE;
         END IF;
-        SET i = i + 1;
+        SET p_i = p_i + 1;
     END WHILE;
 
-    RETURN es_primo;
+    RETURN p_es_primo;
 
 END //
 
 -- Llave 2
 
-CREATE FUNCTION fn_reloj_arena(fecha_ingreso DATE, meses_validez INT)
+CREATE FUNCTION fn_reloj_arena(p_fecha_ingreso DATE, p_meses_validez INT)
 RETURNS VARCHAR(20)
 DETERMINISTIC
 BEGIN
 
-    DECLARE fecha_actual DATE;
-    DECLARE fecha_vencimiento DATE;
+    DECLARE p_fecha_actual DATE;
+    DECLARE p_fecha_vencimiento DATE;
 
-    SET fecha_actual = CURRENT_DATE();
+    IF p_fecha_ingreso IS NULL OR p_meses_validez IS NULL THEN
+        RETURN NULL;
+    END IF;
 
-    SET fecha_vencimiento = DATE_ADD(fecha_ingreso, INTERVAL meses_validez MONTH);
+    SET p_fecha_actual = CURRENT_DATE();
 
-    IF fecha_vencimiento >= fecha_actual THEN
+    SET p_fecha_vencimiento = DATE_ADD(p_fecha_ingreso, INTERVAL p_meses_validez MONTH);
+
+    IF p_fecha_vencimiento >= p_fecha_actual THEN
         RETURN 'Fresco';
     ELSE
         RETURN 'Expirado';
