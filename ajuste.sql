15402646ecd8efead8806927fe74d5a80254f11a
mime0411 <jimenacalderon0411@gmail.com>
2026-04-26 10:33:14 -0600

    Llave 5: fn_escultor implementada

--- ajuste.sql
+++ ajuste.sql
@@ -55,6 +55,79 @@ INSERT INTO inventario_pirata (id, nombre_sucio, categoria, precio_finca, priori
 (5, 'Caramelo_Salado!!', 'Caramelos', 18.00, 7, '2025-12-01', 2),         -- ID 5: VENCIDO.
 (6, 'Gomita_Osa', 'Gomitas', 25.00, 11, '2026-04-10', 8),                  -- ID 6: No es primo.
 (7, '  !!Gomita_Mágica??  ', 'Gomitas', 22.00, 13, '2026-04-01', 10);     -- ID 7: PASA (Primo + Fresco).
+--Llave 5 
+DELIMITER $$
+
+/*CREATE FUNCTION fn_escultor(p_texto TEXT, p_factor DECIMAL(3,2))
+RETURNS TEXT
+DETERMINISTIC
+BEGIN
+    DECLARE v_resultado TEXT;
+    DECLARE v_texto TEXT;
+
+    IF p_texto IS NULL THEN
+        RETURN '';
+    END IF;
+
+    IF p_factor > 1 THEN
+        SET v_texto = UPPER(p_texto);
+        SET v_resultado = CONCAT(v_texto, '_ALTA');
+    ELSE
+        SET v_texto = LOWER(p_texto);
+        SET v_resultado = CONCAT(v_texto, '_BAJA');
+    END IF;
+
+    RETURN v_resultado;
+END$$
+
+DELIMITER ;
+-- Llave 6: función notario
+//DELIMITER $$
+
+CREATE FUNCTION fn_notario(p_texto TEXT)
+RETURNS TEXT
+DETERMINISTIC
+MODIFIES SQL DATA
+BEGIN
+    DECLARE v_usuario VARCHAR(100);
+    DECLARE v_fecha DATETIME;
+    DECLARE v_mensaje TEXT;
+    DECLARE v_funcion VARCHAR(255);
+
+    SET v_usuario = CURRENT_USER();
+    SET v_fecha = NOW();
+    SET v_funcion = 'fn_notario';
+
+    SET v_mensaje = CONCAT('Texto procesado: ', p_texto);
+
+    INSERT INTO logs_hashy(nombre_funcion, fecha_ejecucion, mensaje_accion, usuario_db)
+    VALUES (v_funcion, v_fecha, v_mensaje, v_usuario);
+
+    RETURN p_texto;
+END$$
+
+DELIMITER ;
+--Llave 7 
+DELIMITER $$
+
+CREATE FUNCTION fn_gran_sello(p_texto TEXT)
+RETURNS VARCHAR(255)
+DETERMINISTIC
+BEGIN
+    DECLARE v_hash VARCHAR(255);
+    DECLARE v_texto TEXT;
+
+    IF p_texto IS NULL THEN
+        RETURN '';
+    END IF;
+
+    SET v_texto = p_texto;
+    SET v_hash = MD5(v_texto);
+
+    RETURN v_hash;
+END$$
+
+DELIMITER ;*/
 
 -- ==========================================================
 -- RESULTADO FINAL ESPERADO (VERIFICACIÓN)
